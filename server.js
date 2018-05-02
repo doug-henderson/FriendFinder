365954da300f42fb0de099022144800f3cf81bfd
Doug Henderson <doug@dmhinteractive.com>
2018-05-01 19:26:41 -0500

    updated server.js

--- server.js
+++ server.js
@@ -1,62 +1,31 @@
+// Dependencies
 var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Create an instance of the express app.
 var app = express();
 
-// Set the port of our application
-// process.env.PORT lets the port be set by Heroku
 var PORT = process.env.PORT || 8080;
 
-var exphbs = require("express-handlebars");
+// For serving of static CSS
+//app.use(express.static(__dirname + "/app/css"));
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-var animals = [
-  {
-    animalType: "dog",
-    pet: true,
-    fierceness: 4
-  }, {
-    animalType: "cat",
-    pet: true,
-    fierceness: 10
-  }, {
-    animalType: "giraffe",
-    pet: false,
-    fierceness: 4
-  }, {
-    animalType: "zebra",
-    pet: false,
-    fierceness: 8
-  }, {
-    animalType: "lion",
-    pet: false,
-    fierceness: 10
-  }
-];
+// API and HTML routes
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
 
-app.get("/dog", function(req, res) {
-  // Handlebars requires an object to be sent to the dog.handlebars file. Lucky for us, animals[0] is an object!
-
-  // 1. Send the dog object from the animals array to the dog.handlebars file.
-
-});
-
-app.get("/all-pets", function(req, res) {
-  // Handlebars requires an object to be sent to the index.handlebars file.
-
-  // 2. Send the animals to the index.handlebars file. Remember that animals is an array and not an object.
-
-});
-
-app.get("/all-non-pets", function(req, res) {
-  // Handlebars requires an object to be sent to the index.handlebars file.
-
-  // 3. Send all the animals that are not pets to the index.handlebars file.
-
-});
-
-// Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+	console.log("App listening on PORT: " + PORT);
 });
+
+
+
+
+
+
